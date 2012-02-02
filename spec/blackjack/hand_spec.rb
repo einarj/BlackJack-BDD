18b8428f194965d7c3ec6d332f73e56218d745c3
Einar Jonsson <einar@codelette.com>
2012-02-02 21:45:06 +0000

    Cleaned up hand_spec

--- spec/blackjack/hand_spec.rb
+++ spec/blackjack/hand_spec.rb
@@ -2,23 +2,22 @@ require 'spec_helper'
 
 module BlackJackBDD
   describe Hand do
-    
+   
+    before(:each) do
+      @hand = Hand.new(['S1', 'S2'])
+    end
+
+
     describe "#score" do
-
       it "computes score for two cards of same sort" do
-        hand = Hand.new(['S1', 'S2'])
-        hand.score.should == 3
+        @hand.score.should == 3
       end
-
     end
 
     describe "#cards" do
-
       it "returns its cards as an array" do
-        hand = Hand.new(['S1', 'S2'])
-        hand.cards.should == ['S1', 'S2']
+        @hand.cards.should == ['S1', 'S2']
       end
-
     end
 
   end
